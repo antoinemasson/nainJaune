99af5b353c6be8fcc22fdf5ca4a45a7ce1edea8c
cedric_lefevre <ced1.lef@live.fr>
2014-02-03 09:38:04 +0100

    Case constructeur

---
+++ app/models/Case.java
@@ -0,0 +1,21 @@
+package models;
+
+import java.util.*;
+import javax.validation.*;
+import play.data.validation.Constraints.*;
+
+
+public class Case {
+    @Required
+    private Carte prenante;
+	private double mise;
+	private double cagnotte;
+	private boolean prise;
+
+    public Case (Carte p, double m){
+	prenante=p;
+	mise=m;
+	cagnotte=0;
+    }
+
+};

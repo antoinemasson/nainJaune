af99405525b7ce0f366fd3bb088aa2ad1c78b16d
Antoine <antoine@antoine-VirtualBox.(none)>
2014-02-05 18:44:49 +0100

    Test mise()

--- test/CaseTest.java
+++ test/CaseTest.java
@@ -31,6 +31,9 @@ public class CaseTest {
 		assertThat(c!=null);
 		assertThat(c.get_mise()==1);
 		assertThat(c.get_cagnotte()==0);
+		Case e = new Case(d,5);
+		e.mise(20);
+		assertThat(e.get_cagnotte()==20);
 		//assertThat(c.est_prise());
 		//assertThat(c.get_prenante().equals(d));
 		

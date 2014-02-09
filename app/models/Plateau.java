b51d5ec0709a483b601b287da12170624d0149ff
cedlef <ced1.lef@live.fr>
2014-02-09 18:58:38 +0100

    Plateau toDoble

--- app/models/Plateau.java
+++ app/models/Plateau.java
@@ -40,8 +40,42 @@ public class Plateau{
         if(c==a)return true;        
         a=new Carte("sept","carreaux");
         if(c==a)return true;  
-        return false;
-        
-        
+        return false;        
     }
+    
+    public double toDouble(Carte c){
+        Carte a=new Carte("dix","carreaux");
+        if(c==a){
+            plat[0].mise(2*plat[0].get_cagnotte());
+            return plat[0].get_cagnotte();
+        }
+        
+        a=new Carte("valet","trefle");
+        if(c==a){
+            plat[1].mise(2*plat[1].get_cagnotte());
+            return plat[1].get_cagnotte();
+        }
+
+        a=new Carte("dame","pique");
+        if(c==a){
+            plat[2].mise(2*plat[2].get_cagnotte());
+            return plat[2].get_cagnotte();
+        }
+       
+        a=new Carte("roi","coeur");
+        if(c==a){
+            plat[3].mise(2*plat[3].get_cagnotte());
+            return plat[3].get_cagnotte();
+        }
+              
+        a=new Carte("sept","carreaux");
+        if(c==a){
+            plat[4].mise(2*plat[4].get_cagnotte());
+            return plat[4].get_cagnotte();
+        }
+       
+        return 0;
+        }
+    
+
 }
